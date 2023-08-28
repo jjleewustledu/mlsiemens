fc19441a0d3b4f40d6df847b8604b530e43006a4
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-08-27 20:17:25 -0500

    create() using mlpipeline.ImagingMediator

--- src/+mlsiemens/BiographMMRDevice.m
+++ src/+mlsiemens/BiographMMRDevice.m
@@ -7,22 +7,40 @@ classdef BiographMMRDevice < handle & mlsiemens.BiographDevice
  	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
 
     methods (Static)
+        function this = create(opts)
+            arguments
+                opts.bids_kit mlkinetics.BidsKit {mustBeNonempty}
+                opts.tracer_kit mlkinetics.TracerKit {mustBeNonempty}
+            end
+
+            import mlsiemens.*;
+            bids_med = opts.bids_kit.make_bids_med();
+            counter = opts.tracer_kit.make_handleto_counter();
+            radionuclide = opts.tracer_kit.make_radionuclides();
+            this = BiographMMRDevice( ...
+                calibration=BiographCalibration.create(bids_med, counter, radionuclide), ...
+                data=BiographMMRData.create(bids_med, counter));
+        end
         function this = createFromSession(varargin)
-            this = mlsiemens.BiographMMRDevice( ...
-                'calibration', mlsiemens.BiographCalibration.createFromSession(varargin{:}), ...
-                'data', mlsiemens.BiographMMRData.createFromSession(varargin{:}));
+            import mlsiemens.*;
+            this = BiographMMRDevice( ...
+                calibration=BiographCalibration.createFromSession(varargin{:}), ...
+                data=BiographMMRData.create(varargin{:}));
         end
         function ie = invEfficiencyf(sesd)
-            this = mlsiemens.BiographMMRDevice.createFromSession(sesd);
+            try
+                this = mlsiemens.BiographMMRDevice.create(sesd);
+            catch ME 
+                handwarning(ME)
+                this = mlsiemens.BiographMMRDevice.createFromSession(sesd);
+            end            
             ie = this.invEfficiency_;
         end
     end 	
 
-	properties (Constant)
-        MAX_NORMAL_BACKGROUND = 20 % Bq/mL
- 	end
+    %% PRIVATE
 
-	methods 		  
+	methods (Access = private)
  		function this = BiographMMRDevice(varargin)
  			this = this@mlsiemens.BiographDevice(varargin{:});
  		end
