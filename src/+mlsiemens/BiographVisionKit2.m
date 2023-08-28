f7fa35c401d9dffb3d333851caa4287885c7f096
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-08-27 19:48:42 -0500

    implement mlkinetics.ScannerKit

--- src/+mlsiemens/BiographVisionKit2.m
+++ src/+mlsiemens/BiographVisionKit2.m
@@ -1,38 +1,44 @@
-classdef (Sealed) BiographVisionKit2 < handle & mlsiemens.BiographKit2
-    %% line1
-    %  line2
+classdef (Sealed) BiographVisionKit2 < handle & mlkinetics.ScannerKit
+    %% is an extensible factory making using of the factory method pattern (cf. GoF pp. 90-91, 107). 
     %  
     %  Created 09-Jun-2022 13:53:36 by jjlee in repository /Users/jjlee/MATLAB-Drive/mlsiemens/src/+mlsiemens.
     %  Developed on Matlab 9.12.0.1956245 (R2022a) Update 2 for MACI64.  Copyright 2022 John J. Lee.
     
     methods (Static)
         function this = instance(varargin)
-            %% INSTANCE
-            %  @param optional qualifier is char \in {'initialize' ''}
-            
-            ip = inputParser;
-            addOptional(ip, 'qualifier', '', @ischar)
-            parse(ip, varargin{:})
-            
             persistent uniqueInstance
-            if (strcmp(ip.Results.qualifier, 'initialize'))
-                uniqueInstance = [];
-            end          
             if (isempty(uniqueInstance))
                 this = mlsiemens.BiographVisionKit2();
+                this.install_scanner(varargin{:});
                 uniqueInstance = this;
             else
                 this = uniqueInstance;
+                this.install_scanner(varargin{:});
             end
         end
     end 
 
     methods
-        function dev = make_scanner_device(this)
+        function d = do_make_device(this)
+            this.device_ = mlsiemens.BiographVisionDevice.create( ...
+                bids_kit=this.bids_kit_, ...
+                tracer_kit=this.tracer_kit_);
+            d = this.device_;
+        end
+    end
+
+    %% PROTECTED
+
+    methods (Access = protected)
+        function install_scanner(this, varargin)
+            install_scanner@mlkinetics.ScannerKit(this, varargin{:});
         end
     end
 
     %% PRIVATE
+
+    properties (Access = private)
+    end
 
     methods (Access = private)
         function this = BiographVisionKit2()
