ef131c91a785c25922d4aebe1be2fa46d3f2e746
John Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-08-27 19:34:25 -0500

    hide imagingContext, promote using activity*(), have activity*() delegate to measurements

--- src/+mlsiemens/BiographData.m
+++ src/+mlsiemens/BiographData.m
@@ -7,35 +7,14 @@ classdef BiographData < handle & mlpet.AbstractTracerData
  	%% It was developed on Matlab 9.7.0.1296695 (R2019b) Update 4 for MACI64.  Copyright 2020 John Joowon Lee.
  	
 	properties (Dependent)
-        imagingContext
+        imagingContext % synonym for activityDensity without params, returning ImagingContext2
         radMeasurements
  		visibleVolume
     end
     
-    methods (Static)
-        function this = createFromSession(sesd, varargin)
-            if isa(sesd, 'mlnifti.SessionData')
-                this = mlsiemens.BiographMMRData.createFromSession(sesd, varargin{:});
-                return
-            end
-            if isa(sesd, 'mlpipeline.ImagingMediator')
-                this = mlsiemens.BiographVisionData.createFromSession(sesd, varargin{:});
-                return
-            end
-            this = [];
-        end
-    end
-
-	methods 
-        
-        %% GET/SET
-        
+	methods %% GET/SET 
         function g = get.imagingContext(this)
-            g = this.imagingContext_;
-        end
-        function     set.imagingContext(this, s)
-            assert(isa(s, 'mlfourd.ImagingContext2') || isa(s, 'mlfourd.ImagingFormatContext'))
-            this.imagingContext_ = s;
+            g = this.activityDensity('typ', 'mlfourd.ImagingContext2');
         end
         function g = get.radMeasurements(this)
             g = this.radMeasurements_;
@@ -43,77 +22,39 @@ classdef BiographData < handle & mlpet.AbstractTracerData
         function g = get.visibleVolume(this)
             %% mL
             
-            g = this.imagingContext.imagingFormat;
+            g = this.imagingContext_.imagingFormat;
             g = prod(g.mmppix)/1e3;
         end
-        
-        %%
-        
+    end
+
+    methods
         function a = activity(this, varargin)
             %% Bq
-            %  @param decayCorrected, default := false.
- 			%  @param datetimeForDecayCorrection updates internal.
-            %  @param index0 is numeric.
-            %  @param indexF is numeric.
-            %  @param timeAveraged is logical.
-            %  @param volumeAveraged is logical.
-            %  @param diff is logical.
+            %  decayCorrected logical = false.
+ 			%  datetimeForDecayCorrection datetime = NaT, updates internal.
+            %  index0 double {@isnumeric} = this.index0
+            %  indexF double {@isnumeric} = this.indexF
+            %  timeAveraged logical = false
+            %  volumeAveraged logical = false.
+            %  diff logical = false.
+            %  uniformTimes logical = false, applicable only if volumeAveraged.
+            %  typ text = 'single'.
             
             a = this.activityDensity(varargin{:})*this.visibleVolume;
         end
         function a = activityDensity(this, varargin)
             %% Bq/mL
-            %  @param decayCorrected, default := false.
- 			%  @param datetimeForDecayCorrection updates internal.
-            %  @param index0 is numeric.
-            %  @param indexF is numeric.
-            %  @param timeAveraged is logical, default false.
-            %  @param volumeAveraged is logical, default false.
-            %  @param diff is logical, default false.
-            %  @param uniformTimes is logical, default false.  Applicable only if volumeAveraged.
+            %  decayCorrected logical = false.
+ 			%  datetimeForDecayCorrection datetime = NaT, updates internal.
+            %  index0 double {@isnumeric} = this.index0
+            %  indexF double {@isnumeric} = this.indexF
+            %  timeAveraged logical = false
+            %  volumeAveraged logical = false.
+            %  diff logical = false.
+            %  uniformTimes logical = false, applicable only if volumeAveraged.
+            %  typ text = 'single'.
             
-            ip = inputParser;
-            addParameter(ip, 'decayCorrected', false, @islogical)
-            addParameter(ip, 'datetimeForDecayCorrection', NaT, @(x) isnat(x) || isdatetime(x))
-            addParameter(ip, 'index0', this.index0, @isnumeric)
-            addParameter(ip, 'indexF', this.indexF, @isnumeric)
-            addParameter(ip, 'timeAveraged', false, @islogical)
-            addParameter(ip, 'volumeAveraged', false, @islogical)
-            addParameter(ip, 'diff', false, @islogical)
-            addParameter(ip, 'uniformTimes', false, @islogical)
-            parse(ip, varargin{:})
-            ipr = ip.Results;
-            
-            if isdatetime(ipr.datetimeForDecayCorrection) && ...
-                    ~isnat(ipr.datetimeForDecayCorrection)
-                this.datetimeForDecayCorrection = ipr.datetimeForDecayCorrection;
-            end  
-            if ~ipr.decayCorrected && this.decayCorrected
-                this = this.decayUncorrect();
-            end
-            
-            that = copy(this);
-            if ipr.index0 ~= this.index0 || ipr.indexF ~= this.indexF
-                that = that.selectIndex0IndexF(ipr.index0, ipr.indexF);
-            end
-            if ipr.timeAveraged
-                that.imagingContext_ = that.imagingContext_.timeAveraged();
-            end
-            if ipr.volumeAveraged && ndims(that.imagingContext_) >= 3 %#ok<ISMAT>
-                that.imagingContext_ = that.imagingContext_.volumeAveraged();                
-                a = that.imagingContext_.imagingFormat.img;
-                if ipr.uniformTimes
-                    a = interp1(this.timesMid, a, this.timeInterpolants);
-                end
-                if ipr.diff
-                    a = diff(a);
-                end
-            else                
-                if ipr.diff
-                    that.imagingContext_ = diff(that.imagingContext_);
-                end
-                a = that.imagingContext_.imagingFormat.img;
-            end
+            a = this.measurement(varargin{:});
         end
         function that = blurred(this, varargin)
             that = copy(this);
@@ -121,19 +62,21 @@ classdef BiographData < handle & mlpet.AbstractTracerData
         end
         function c = countRate(this, varargin)
             %% Bq/mL, decay-corrected.
-            %  @param decayCorrected, default := true.
- 			%  @param datetimeForDecayCorrection updates internal.
-            %  @param index0 is numeric.
-            %  @param indexF is numeric.
-            %  @param timeAveraged is logical.
-            %  @param volumeAveraged is logical.
-            %  @param diff is logical.
+            %  decayCorrected logical = false.
+ 			%  datetimeForDecayCorrection datetime = NaT, updates internal.
+            %  index0 double {@isnumeric} = this.index0
+            %  indexF double {@isnumeric} = this.indexF
+            %  timeAveraged logical = false
+            %  volumeAveraged logical = false.
+            %  diff logical = false.
+            %  uniformTimes logical = false, applicable only if volumeAveraged.
+            %  typ text = 'single'.
             
             c = this.activityDensity('decayCorrected', true, varargin{:});
         end
         function this = decayCorrect(this)
             if ~this.decayCorrected
-                ifc = this.imagingContext.imagingFormat;
+                ifc = this.imagingContext_.imagingFormat;
                 mat = this.reshape_native_to_2d(ifc.img);
                 mat = mat .* this.decayCorrectionFactors;
                 ifc.img = this.reshape_2d_to_native(mat);
@@ -161,7 +104,7 @@ classdef BiographData < handle & mlpet.AbstractTracerData
         end
         function this = decayUncorrect(this)
             if this.decayCorrected
-                ifc = this.imagingContext.imagingFormat;
+                ifc = this.imagingContext_.imagingFormat;
                 mat = this.reshape_native_to_2d(ifc.img);
                 mat = mat ./ this.decayCorrectionFactors;
                 ifc.img = this.reshape_2d_to_native(mat);
@@ -199,8 +142,8 @@ classdef BiographData < handle & mlpet.AbstractTracerData
             this.imagingContext_ = mlfourd.ImagingContext2(varargin{:});
             this = this.decayUncorrect();
         end
-        function img  = reshape_native_to_2d(this, img)
-            sz  = size(this.imagingContext.imagingFormat);
+        function img = reshape_native_to_2d(this, img)
+            sz  = size(this.imagingContext_.imagingFormat);
             switch length(sz)
                 case 2
                     return
@@ -213,8 +156,8 @@ classdef BiographData < handle & mlpet.AbstractTracerData
                     error('mlsiemens:RuntimeError', 'BiographData.reshape_native_to_2d')
             end
         end
-        function img  = reshape_2d_to_native(this, img)
-            sz  = size(this.imagingContext.imagingFormat);
+        function img = reshape_2d_to_native(this, img)
+            sz  = size(this.imagingContext_.imagingFormat);
             switch length(sz)
                 case 2
                     return
@@ -238,7 +181,7 @@ classdef BiographData < handle & mlpet.AbstractTracerData
             parse(ip, Dt, varargin{:})
             assert(isscalar(this.halflife))
             
-            ifc = this.imagingContext.imagingFormat;
+            ifc = this.imagingContext_.imagingFormat;
             ifc.img = ifc.img * 2^(-Dt/this.halflife);            
             this.imagingContext_ = mlfourd.ImagingContext2(ifc, ...
                 'fileprefix', sprintf('%s_shiftWorldlines%g', ifc.fileprefix, Dt));
@@ -283,9 +226,8 @@ classdef BiographData < handle & mlpet.AbstractTracerData
                 this.radMeasurements_ = mlpet.CCIRRadMeasurements.createFromDate(this.datetimeMeasured);
             end
             this.datetimeMeasured = this.datetimeMeasured - this.clocksTimeOffsetWrtNTS;
-        end
-         
-        function sec  = clocksTimeOffsetWrtNTS(this)
+        end         
+        function sec = clocksTimeOffsetWrtNTS(this)
             try
                 sec = seconds(this.radMeasurements_.clocks.TimeOffsetWrtNTS____s('mMR console'));
             catch ME
@@ -293,26 +235,105 @@ classdef BiographData < handle & mlpet.AbstractTracerData
                 sec = seconds(this.radMeasurements_.clocks.TIMEOFFSETWRTNTS____S('mMR console'));
             end
         end 
-        function this = selectIndex0IndexF(this, index0, indexF)
-            fdfp = this.imagingContext.imagingFormat;
-            switch fdfp.ndims
-                case 2
-                    fdfp.img = fdfp.img(index0:indexF);
-                case 3
-                    fdfp.img = fdfp.img(:,:,index0:indexF);
-                case 4
-                    fdfp.img = fdfp.img(:,:,:,index0:indexF);
-                otherwise
-                    error('mlsiemens:ValueError', 'BiographData.selectIndex0IndexF')
-            end
-            this.imagingContext_ = mlfourd.ImagingContext2(fdfp);
-        end
         function that = copyElement(this)
             %%  See also web(fullfile(docroot, 'matlab/ref/matlab.mixin.copyable-class.html'))
             
             that = copyElement@matlab.mixin.Copyable(this);
             that.imagingContext_ = copy(this.imagingContext_);
             that.radMeasurements_ = copy(this.radMeasurements_);
+        end
+        function ic = ensure_times_mid(this, ic)
+            arguments
+                this mlsiemens.BiographData
+                ic mlfourd.ImagingContext2
+            end
+
+            j = ic.json_metadata;
+            if isfield(j, 'timesMid')
+                return
+            end
+            ic.addJsonMetadata(struct("timeMid", this.timesMid));
+            ic.addJsonMetadata(struct("timeMidUnits", "seconds"));
+        end
+        function m = measurement(this, varargin)
+            %% Bq/mL
+
+            ip = inputParser;
+            ip.KeepUnmatched = true;
+            ip.PartialMatching= false;
+            addParameter(ip, 'decayCorrected', false, @islogical)
+            addParameter(ip, 'datetimeForDecayCorrection', NaT, @(x) isnat(x) || isdatetime(x))
+            addParameter(ip, 'index0', this.index0, @isnumeric)
+            addParameter(ip, 'indexF', this.indexF, @isnumeric)
+            addParameter(ip, 'timeAveraged', false, @islogical)
+            addParameter(ip, 'volumeAveraged', false, @islogical)
+            addParameter(ip, 'diff', false, @islogical)
+            addParameter(ip, 'uniformTimes', false, @islogical)
+            addParameter(ip, 'typ', 'single', @istext)
+            parse(ip, varargin{:})
+            ipr = ip.Results;
+            
+            if ~isnat(ipr.datetimeForDecayCorrection)
+                this.datetimeForDecayCorrection = ipr.datetimeForDecayCorrection;
+            end
+            if ipr.decayCorrected && ~this.decayCorrected
+                this.decayCorrect(); % handle
+                this.decayCorrected_ = true;
+            end
+            if ~ipr.decayCorrected && this.decayCorrected
+                this.decayUncorrect(); % handle
+                this.decayCorrected_ = false;
+            end 
+            
+            ic = copy(this.imagingContext_);
+            ic = this.ensure_times_mid(ic);
+            if ipr.index0 ~= this.index0 || ipr.indexF ~= this.indexF
+                ic = this.selectIndex0IndexF(ic, ipr.index0, ipr.indexF);
+            end
+            if ipr.timeAveraged
+                ic = ic.timeAveraged();
+            end
+            if ipr.volumeAveraged && ndims(ic) >= 3 %#ok<ISMAT>
+                ic = ic.volumeAveraged();  
+                if ipr.uniformTimes
+                    taus_ = asrow(this.timeInterpolants(2:end) - this.timeInterpolants(1:end-1));
+                    taus_ = [taus_, taus_(end)];
+
+                    ifc = ic.imagingFormat;
+                    ifc.img = interp1(this.timesMid, ifc.img, this.timeInterpolants);
+                    ifc.json_metadata.taus = taus_;
+                    ifc.json_metadata.times = this.timeInterpolants;
+                    ifc.json_metadata.timesMid = this.timeInterpolants + taus_/2;
+                    ifc.json_metadata.timeUnit = "second";
+                    ic = mlfourd.ImagingContext2(ifc);                    
+                end
+            end
+            if ipr.diff
+                ic = diff(ic);
+            end
+            m = imagingType(ipr.typ, ic);
+            % m = m/this.branchingRatio; % already performed by Siemens scanners
+        end
+        function ic = selectIndex0IndexF(this, ic, index0, indexF)
+            arguments
+                this mlsiemens.BiographData %#ok<INUSA>
+                ic mlfourd.ImagingContext2
+                index0 double {mustBeInteger}
+                indexF double {mustBeInteger}
+            end
+
+            ifc = ic.imagingFormat;
+            switch ifc.ndims
+                case 2
+                    ifc.img = ifc.img(index0:indexF);
+                case 3
+                    ifc.img = ifc.img(:,:,index0:indexF);
+                case 4
+                    ifc.img = ifc.img(:,:,:,index0:indexF);
+                otherwise
+                    error('mlsiemens:ValueError', 'BiographData.selectIndex0IndexF')
+            end
+            ic = mlfourd.ImagingContext2(ifc);
         end
  	end 
 
